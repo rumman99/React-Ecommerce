462f7325d97714e383366801ea03317ffad2d538
rumman99 <tasnimrumman369@gmail.com>
2024-01-19 02:30:32 +0600

    fix vercel deploy

---
+++ client/src/firebase_config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBrX9z0JnpRgpy6fZNcMZJhzLrV8SQH3Bc",
+  authDomain: "ema-john-ca642.firebaseapp.com",
+  projectId: "ema-john-ca642",
+  storageBucket: "ema-john-ca642.appspot.com",
+  messagingSenderId: "1040121246458",
+  appId: "1:1040121246458:web:06572cbb37c03104c94f43"
+};
+
+export default firebaseConfig;
